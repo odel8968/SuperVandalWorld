e14ad7a1048abce16fed9b98e238e82335465f28
jmharris7755 <43102944+jmharris7755@users.noreply.github.com>
2020-09-27 02:11:51 -0700

    1st iteration of MVP
    Rough design of MVP. Generic Start Menu created. Level 1 has place-holder sprites for Player(large circle), Enemies(square and triangle), pick-ups(small circles). and main ground was created with a generic tile map. Blank Test Level created.

---
+++ SuperVandalWorld/Assets/src/Justin/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D PlayerRb;
+    
+    [SerializeField]
+    private float movementSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+
+        PlayerRb.velocity = new Vector2(horizontal * movementSpeed, PlayerRb.velocity.y);
+    }
+}
