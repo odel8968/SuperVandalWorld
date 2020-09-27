48ce3b5f32e8326fef5c9018fb0739765cec8186
Keller <keeperkell@gmail.com>
2020-09-27 13:15:30 -0700

    Created pickupsManager script
    Created pickupsManager script

---
+++ SuperVandalWorld/Assets/tst/Keller/pickupsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupsManager : MonoBehaviour
+{
+    void updateScore(){
+
+    }
+
+    void removeAsset(){
+
+    }
+
+    void triggerSound(){
+        
+    }
+}
+
+
+public class powerUps : pickupsManager
+{
+    //subclass of pickupsManager defining specific functions of powerups
+
+}
+
+public class items : pickupsManager
+{
+    //subclass of pickupsManager defining specific functions of items
+
+}
