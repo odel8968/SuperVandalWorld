01ec7f6d2748211fc9eeb7e20bdc9de571e46581
Odell <54610900+fungus@users.noreply.github.com>
2020-09-27 21:28:21 -0700

    Pause menu added
    Press escape to open pause menu

---
+++ SuperVandalWorld/Assets/src/Ben/PauseMenu.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+	GameObject[] pauseObjects;
+
+	void Start()
+	{
+		Time.timeScale = 1;
+		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		//uses the p button to pause and unpause the game
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+			}
+			else if (Time.timeScale == 0)
+			{
+				Debug.Log("high");
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
+	}
+
+
+	//controls the pausing of the scene
+	public void pauseControl()
+	{
+		if (Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			showPaused();
+		}
+		else if (Time.timeScale == 0)
+		{
+			Time.timeScale = 1;
+			hidePaused();
+		}
+	}
+
+	//shows objects with ShowOnPause tag
+	public void showPaused()
+	{
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(true);
+		}
+	}
+
+	//hides objects with ShowOnPause tag
+	public void hidePaused()
+	{
+		foreach (GameObject g in pauseObjects)
+		{
+			g.SetActive(false);
+		}
+	}
+
+	//loads inputted level
+	public void LoadLevel(string level)
+	{
+		//Application.LoadLevel(level);
+	}
+
+	public void HelpMenu()
+	{
+		Debug.Log("Help command received");
+	}
+
+	public void QuitGame()
+    {
+		Debug.Log("Quit command received");
+		Application.Quit();
+    }
+}
