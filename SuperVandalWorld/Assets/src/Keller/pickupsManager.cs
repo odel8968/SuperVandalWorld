c913b78f017ff735420d192c426d004bbfdb9f27
Keller <keeperkell@gmail.com>
2020-09-29 14:06:06 -0700

    created item prefabs and scripts
    Created prefabs for items and defined functions for pickups

---
+++ SuperVandalWorld/Assets/src/Keller/pickupsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupsManager : MonoBehaviour
+{
+    public  void onTriggerEnter2D(Collision2D trig){
+        //on trigger with pickup
+        Debug.Log("Collision with " + trig.gameObject.tag);
+
+        removeAsset(trig);
+        updateScore(trig);
+        triggerSound(trig);
+    }
+
+    public  void updateScore(Collision2D trig){
+        //call GUI manager's function for score to be updated
+
+    }
+
+    public  void removeAsset(Collision2D trig){
+        //remove asset from level via destroy function
+        Destroy(trig.gameObject);
+    }
+
+    public  void triggerSound(Collision2D trig){
+        //call Sound managers function to play sound
+    }
+}
