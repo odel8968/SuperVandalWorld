b68f738b1a5a2d7773f4fdb6ac6330e9f7e77526
Odell <54610900+fungus@users.noreply.github.com>
2020-09-30 18:39:01 -0700

    Sound manager object and script added

---
+++ SuperVandalWorld/Assets/src/Ben/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+
+
+    private AudioSource musicSource;
+    private AudioSource sfxSource;
+}
