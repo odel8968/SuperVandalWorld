19f309e4179434db8bc42a47af4f97cf513b963a
jmharris7755 <43102944+jmharris7755@users.noreply.github.com>
2020-10-07 15:04:00 -0700

    Added Tests for MainCamer
    Test to check if camera is set to follow player, x-offset, y-offset, and camera speed are reading correctly in code.

---
+++ SuperVandalWorld/Assets/tst/Justin/PlatformMovementTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlatformMovementTest
+    {
+        private bool sceneLoaded;
+
+
+        [OneTimeSetUp]
+        public void loadedLevel()
+        {
+
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+            SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
+        }
+
+        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+
+            sceneLoaded = true;
+        }
+
+        [UnityTest]
+        public IEnumerator MovingPlatformSpeed()
+        {
+            yield return new WaitWhile(()=>sceneLoaded == false);
+            var platform = GameObject.Find("MovingPlatform1").GetComponent<MovingPlatform>();
+
+            Assert.That(platform.moveSpeed, Is.EqualTo(5f));
+        }
+
+        [UnityTest]
+        public IEnumerator MovingPlatformToPointB()
+        {
+            yield return new WaitWhile(()=>sceneLoaded == false);
+            var platform = GameObject.Find("MovingPlatform1/MovingRockPlatform1");
+            var pointB = GameObject.Find("MovingPlatform1/PointB").transform.position.x;
+            var pointA = GameObject.Find("MovingPlatform1/PointA").transform.position.x;
+            var platSpeed = GameObject.Find("MovingPlatform1").GetComponent<MovingPlatform>();
+
+            //while(pPos < pointB)
+           // {
+           //     pPos = platform.transform.position.x;
+           //     Debug.Log(pPos); 
+           // }
+            
+
+            //yield return new WaitForSeconds((20));
+
+            Assert.That(platform, Is.EqualTo(pointB));
+        } 
+
+        
+    }
+}
