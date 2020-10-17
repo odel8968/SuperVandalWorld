19776a795f52fa6b036fa2665eaf05b4aa82c00b
Keller <keeperkell@gmail.com>
2020-10-17 03:40:18 -0700

    Added use of ability powerAxe
    Created methods for powerAxe allowing for character to throw axes.

--- SuperVandalWorld/Assets/src/Keller/powerAxe.cs
+++ SuperVandalWorld/Assets/src/Keller/powerAxe.cs
@@ -5,5 +5,43 @@ using UnityEngine;
 
 public class powerAxe : PowerUp
 {
-    
+    private float projVel = 8.0f;       //projectile velocity
+    private float projDelay = 1.0f;     //delay between projectile fires
+    private float projNextTime = 0.0f;  //next time projectile can be fired
+    public GameObject axeProj;
+    public KeyCode abilityKey = KeyCode.J;
+
+
+    public void addAxeForce(GameObject axe){
+        axe.GetComponent<Rigidbody2D>().velocity = new Vector2(projVel, 1.5f * projVel);
+    }
+
+    public void removeProjectile(GameObject axe){
+ 
+        Destroy(axe, 5.0f);
+    }
+
+    void OnCollisionEnter(Collision col){
+        Debug.Log(col.gameObject.tag);
+        Destroy(col.gameObject);
+    }
+
+    void Update(){
+        GameObject proj = null;
+
+        if(Input.GetKeyDown(abilityKey)){
+            if(proj == null){
+                if(Time.time >= projNextTime){
+                    projNextTime = Time.time + projDelay;
+
+                    proj = GameObject.Instantiate(axeProj, transform.position, transform.rotation);
+                    addAxeForce(proj);
+                    removeProjectile(proj);
+                }
+            }      
+        }   
+    }
+
+
+
 }
