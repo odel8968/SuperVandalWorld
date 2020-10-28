ded433edc00ecc7c47d34025b328bd17ebda8b4a
John Cambareri <webbcamjl15@yahoo.com>
2020-10-28 15:33:49 -0700

    Implemented Demo Character
    Added a demo version of the player character. He's very basic, and as of right now, he's still active when the player is.

--- SuperVandalWorld/Assets/src/John/Character_Movement.cs
+++ SuperVandalWorld/Assets/src/John/Character_Movement.cs
@@ -9,8 +9,10 @@ public class Character_Movement : MonoBehaviour
     public float speed;
     public float jumpForce;
     public static bool hasAbility;
-
-    bool isGrounded = false;
+    public static int MAX_JUMPS = 2;
+    protected int jumps_taken;
+    public int jumps_allowed;
+    protected bool isGrounded = false;
     public float checkGroundRadius;
     public LayerMask groundLayer; //For this to work, all ground needs to be on its own layer
 
@@ -20,28 +22,39 @@ public class Character_Movement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         bc = GetComponent<BoxCollider2D>();
         hasAbility = false;
+        jumps_taken = 0;
+        jumps_allowed = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    public virtual void Move()
+    {
         float x = Input.GetAxisRaw("Horizontal");
         float moveBy = x * speed;
         rb.velocity = new Vector2(moveBy, rb.velocity.y);
+        isGrounded = CheckIfGrounded();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             isGrounded = CheckIfGrounded();
+            if (isGrounded)
+                jumps_taken = 0;
             Jump(isGrounded);
         }
-
     }
 
     public void Jump(bool is_grounded)
     {
-        if (is_grounded)
+        //Debug.Log("is_grounded = " + is_grounded + ", jumps_taken = " + jumps_taken + ", jumps_allowed = " + jumps_allowed);
+        if (is_grounded || jumps_taken < jumps_allowed)
         {
             Debug.Log("Jumping");
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumps_taken++;
         }
         else
         {
@@ -49,19 +62,27 @@ public class Character_Movement : MonoBehaviour
         }
     }
 
-    bool CheckIfGrounded()
+    protected bool CheckIfGrounded()
     {
         RaycastHit2D RCH2D = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0f, Vector2.down, .1f, groundLayer);
         return RCH2D.collider != null; //null if not grounded; not null if grounded
     }
 
+    protected void IncreaseMaxJumps()
+    {
+        if (jumps_allowed + 1 > MAX_JUMPS)
+        {
+            return;
+        }
+
+        jumps_allowed++;
+    }
+
     /*void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Pickup")
+        if (other.gameObject.tag == "Ground")
         {
-            Debug.Log("Pickup obtained.");
-            Destroy(other.gameObject);
-
+            jumps_taken = 0;
         }
     }*/
 }
---
+++ SuperVandalWorld/Assets/src/John/Demo_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo_Movement : Character_Movement
+{
+    private int seed;
+    private int timer = 0;
+    private bool left = false;
+
+    public override void Move()
+    {
+        if (!left)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        
+        timer++;
+        if (timer > 100)
+        {
+            left = !left;
+            timer = 0;
+        }
+
+        isGrounded = CheckIfGrounded();
+        if (isGrounded)
+        {
+            jumps_taken = 0;
+            Jump(isGrounded);
+        }
+    }
+}
