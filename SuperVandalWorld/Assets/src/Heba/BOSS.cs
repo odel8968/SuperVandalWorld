cc5a94e1303a3a97d90c95fe813c7b44585b8e8a
HebaAljabrine <Alja3818@vandals.uidaho.edu>
2020-10-28 17:37:13 -0700

    BOSS-Moving

--- SuperVandalWorld/Assets/src/Heba/BOSS.cs
+++ SuperVandalWorld/Assets/src/Heba/BOSS.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class BOSS : MonoBehaviour
 {
+    public float speed;    
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 moveVelocity;
+    public float curTime;
+    public float moveTime = 3;
+    public float direction = 1;
+
     // Start is called before the first frame update
     void Start()
     {
+         rb = GetComponent<Rigidbody2D>();  
+       animator = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        curTime += Time.deltaTime;
+        if(curTime >= moveTime)
+        {
+            direction *= -1;
+            curTime = 0;
+        }
+        moveVelocity = new Vector2(direction, 0) * speed;
         
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
