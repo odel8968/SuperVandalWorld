d3925a6464275fa6c74d42ab1b0305cc023e014d
jmharris7755 <43102944+jmharris7755@users.noreply.github.com>
2020-11-11 12:31:06 -0800

    Checkpoint Manager
    Adjusted cm script

--- SuperVandalWorld/Assets/src/Justin/CheckpointManager.cs
+++ SuperVandalWorld/Assets/src/Justin/CheckpointManager.cs
@@ -32,10 +32,12 @@ public class CheckpointManager : MonoBehaviour
         //Upon loading to the next level
         if(lvlLoaded.nxtLevel)
         {
-            //Destroy this instance of the CheckpointManager
+            /*//Destroy this instance of the CheckpointManager
             Destroy(instance);
             //Destroy Checkpoint Manager GameObject
-            Destroy(this.gameObject);
+            Destroy(this.gameObject);*/
+            //reset checkpoint position to beginning of next level
+            lastCheckPointPos = new Vector2(0,0);
             //Set the state of the next level loaded back to false to indicate nxlLevel = current level
             lvlLoaded.SetToCurrentLevel();
         }
