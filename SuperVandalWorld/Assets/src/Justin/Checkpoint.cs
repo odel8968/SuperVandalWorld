b104ecdc5bfa4a1692b7a48f2a65f7f134050448
jmharris7755 <43102944+jmharris7755@users.noreply.github.com>
2020-11-13 20:49:26 -0800

    Additional work on CheckPoints

--- SuperVandalWorld/Assets/src/Justin/Checkpoint.cs
+++ SuperVandalWorld/Assets/src/Justin/Checkpoint.cs
@@ -6,9 +6,15 @@ public class Checkpoint : MonoBehaviour
 {
     //reference to checkpoint manager
     private CheckpointManager cm;
+    private BoxCollider2D coll;
+  
+    //variable to trigger animation
+    private Animator anim;
     void Start()
     {
         cm = GameObject.FindGameObjectWithTag("CM").GetComponent<CheckpointManager>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
     }
     //call when player collides with checkpoint
     void OnTriggerEnter2D(Collider2D other)
@@ -16,7 +22,14 @@ public class Checkpoint : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             cm.lastCheckPointPos = transform.position;
-            Destroy(gameObject);
+            anim.enabled = true;
+            Invoke("DisableAnimation", 1.5f * Time.deltaTime);
+            coll.enabled = false;
         }
+    }
+
+    void DisableAnimation()
+    {
+        anim.enabled = false;
     }
 }
